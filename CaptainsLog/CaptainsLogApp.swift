efd0873f93906520e780adf12efd743a0024aa04
danielios-hub <daniel.ios.dgp@gmail.com>
2025-04-05 18:14:23 +0200

    Initial Commit

---
+++ CaptainsLog/CaptainsLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptainsLogApp.swift
+//  CaptainsLog
+//
+//  Created by Daniel Gallego Peralta on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CaptainsLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptainsLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaptainsLog
+//
+//  Created by Daniel Gallego Peralta on 5/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
